11d4e3f2bcdae7b3169e31be42f7cb9a5499f8bd
ikn1062 <ishaannarain2022@u.northwestern.edu>
2022-02-24 21:56:39 -0600

    Comment for code

--- 1-DOF Prototype/ODRIVE/Virtual Wall Demo/impulse_wall.py
+++ 1-DOF Prototype/ODRIVE/Virtual Wall Demo/impulse_wall.py
@@ -1,37 +1,44 @@
-import time
 import odrive as o
 from odrive.enums import *
 
 
-def main():
+def impulse_wall_demo():
+    """
+    Impulse Wall Demo for virtual wall for RDS Wand 1-DOF Subsystem Prototype
+    :return: Torque commands to motor to generate virtual wall
+    """
+    # Connecting to an ODrive
     print("Finding an ODRIVE...")
     m = o.find_any()
     m1 = m.axis1
     print("ODRIVE Connected...")
 
+    # Setting initial torque to be 0 and states to run Torque commands
     m1.controller.input_torque = 0.0
     m1.controller.config.control_mode = CONTROL_MODE_TORQUE_CONTROL
     m1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
+    # Set up initial constants for the mass spring damper system
     wall = 0.5
     kp = 7.5
     kv = 0.05
 
-    t = 0
-    impulse_time = 0.01
-    dt = 0.005
-
     # Impulse force and impulse start is turned off
     impulse_wall = False
     impulse_enabled = False
+
+    # Set up initial constants for the impulse wall force
+    t = 0
+    impulse_time = 0.01
+    dt = 0.005
     m = 0.00005
     const_v = 0
 
     while True:
+        # Generates position and velocity values
         p = m1.encoder.pos_estimate
         v = m1.encoder.vel_estimate
         print("pos: %f, vel: %f" % (p, v))
-        torque = 0.0
         if p >= wall:
             # If within wall send the spring mass damper, send spring mass torque
             torque = -1 * kp * (p - wall) + -1 * kv * v
@@ -41,7 +48,6 @@ def main():
                 impulse_wall = True
                 impulse_enabled = True
                 const_v = v + 0.0
-                # impulse_time = const_v / 8
 
             if impulse_enabled:
                 # Send impulse force if impulse false is positive
@@ -58,8 +64,8 @@ def main():
             if p < wall - 0.08:
                 # Resets impulse a certain away from the wall
                 impulse_wall = False
-        print("Torque: %f" % torque)
+
         m1.controller.input_torque = torque
 
 
-main()
+impulse_wall_demo()
